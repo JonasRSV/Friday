71da52a598fd62884eda117e81fd23c5491b55d7
Jonas Valfridsson <jonas@valfridsson.net>
2020-11-28 17:41:28 +0100

    Fix path issue

--- mm/tools/friday_inspect.py
+++ mm/tools/friday_inspect.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+
+# Some systems dont use the launching directory as root
+sys.path.append(os.getcwd())
+
 import models.shared.augmentation as augmentation
 import simpleaudio
 import tensorflow as tf
