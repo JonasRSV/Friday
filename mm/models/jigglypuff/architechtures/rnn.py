588e741148d6acb391ba9b307b8c00fdfa5577b8
Jonas Valfridsson <jonas@valfridsson.net>
2021-02-14 14:14:48 +0100

    md

--- mm/models/jigglypuff/architechtures/rnn.py
+++ mm/models/jigglypuff/architechtures/rnn.py
@@ -13,6 +13,7 @@ def rnn(x: tf.Tensor,
     cells = [
         tf.contrib.rnn.LSTMCell(128),
         tf.contrib.rnn.LSTMCell(128),
+        tf.contrib.rnn.LSTMCell(128),
     ]
     # The second output is the last state and we will no use that
     x = tf.keras.layers.RNN(cells, return_sequences=True)(x, training=mode == tf.estimator.ModeKeys.TRAIN)
