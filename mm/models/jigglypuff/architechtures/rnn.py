20725e4dc7d2298ad6e7389e8cc7f27ce13ad2f2
Jonas Valfridsson <jonas@valfridsson.net>
2021-02-14 14:18:18 +0100

    md

--- mm/models/jigglypuff/architechtures/rnn.py
+++ mm/models/jigglypuff/architechtures/rnn.py
@@ -11,14 +11,14 @@ def rnn(x: tf.Tensor,
     print("x", x.shape)
 
     cells = [
-        tf.contrib.rnn.LSTMCell(128),
-        tf.contrib.rnn.LSTMCell(128),
-        tf.contrib.rnn.LSTMCell(128),
+        tf.contrib.rnn.LSTMCell(256),
+        tf.contrib.rnn.LSTMCell(256),
+    #    tf.contrib.rnn.LSTMCell(128),
     ]
     # The second output is the last state and we will no use that
     x = tf.keras.layers.RNN(cells, return_sequences=True)(x, training=mode == tf.estimator.ModeKeys.TRAIN)
     print("x", x)
-    x = tf.keras.layers.Dense(128, activation='tanh')(x)
+    #x = tf.keras.layers.Dense(128, activation='tanh')(x)
     print("x", x)
     logits = tf.keras.layers.Dense(num_phonemes, activation=None)(x)
 
