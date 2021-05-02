73b0d10936ef3ec94d787603d4ed1beda7c45d05
Jonas Valfridsson <jonas@valfridsson.net>
2021-05-02 14:38:54 +0200

    coeff

--- mm/models/bulbasaur/architechtures/kaggle.py
+++ mm/models/bulbasaur/architechtures/kaggle.py
@@ -13,7 +13,7 @@ def kaggle_cnn(x: tf.Tensor,
                                        kernel_size=(7, 3),
                                        activation=tf.nn.relu,
                                        name="kaggle_cnn_1_c")(x)
-        print("x", x)
+        #print("x", x)
         #x = tf.compat.v1.layers.MaxPooling2D(pool_size=(1, 3), strides=(1, 3),
                                             # name="kaggle_cnn_1_m")(x)
         x = tf.compat.v1.layers.MaxPooling2D(pool_size=(1, 3), strides=(1, 1), name="kaggle_cnn_1_m")(x)
